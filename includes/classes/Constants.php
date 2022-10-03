cdebb774c36059de36fd563740d4fb897db00eb8
DALMIK <89015969+DALMIK@users.noreply.github.com>
2022-10-03 10:22:58 +0530

    Update Constants.php

--- includes/classes/Constants.php
+++ includes/classes/Constants.php
@@ -1,12 +1,5 @@
 <?php
 class Constants {
-    public static $firstNameCharacters = "<span>Your first name must be between 2 and 25 characters</span>";
-    public static $lastNameCharacters = "Your last name must be between 2 and 25 characters";
-    public static $usernameCharacters = "Your username must be between 2 and 25 characters";
-    public static $usernameTaken = "Username already in use";
-    public static $emailInvalid = "Invalid email    ";
-    public static $emailTaken = "email already in use";
-    public static $passwordLength = "Your Password must be of 2 and 10 characters";
     public static $loginFailed = "<span style='color:red'>Your username and Password are incorrect</span>";
 }
 ?>
